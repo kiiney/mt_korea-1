3d3037c16fb65873de33986fb23168d1ba698a9a
hocolally <hocolally@gmail.com>
2020-10-16 09:33:58 +0900

    first commit

---
+++ mt_korea/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include
+
+from board import views as boardview
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', boardview.index),
+    path('board/', include('board.urls'))
+]
