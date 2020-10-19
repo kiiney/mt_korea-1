95ac9b1d8f71640f4e83e7cf4dd285ebc1471b9c
kayfour <kayfour@hanmail.net>
2020-10-19 17:40:48 +0900

    db2

--- scrap_code/mtdetail.py
+++ scrap_code/mtdetail.py
@@ -5,24 +5,17 @@ import pandas as pd
 
 header = {'User-Agent' : 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36'}
 b=[]
-f = open('mt_he.csv', 'w', encoding='utf-8')
+# f = open('mt_de.csv', 'w', encoding='utf-8')
 for num in range (1,101):
     a=[]
     url = f'http://mtweather.nifos.go.kr/famous/mountainOne?stnId={num}'
     res = requests.get(url, headers=header).json()
     cont = res.get("famousMTSDTO")    
     mtname=cont.get("stnName")
-    #a.append(mtname)  
-    address=cont.get("address")
-    #a.append(address)
-    lat=cont.get("fmtLat")
-    #a.append(lat)
-    lon=cont.get("fmtLon")
-    #a.append(lon)
-    #desc =cont.get("description")
-    #a.append(desc)
-    f.write(str(num)+","+mtname+","+address+","+lat+","+lon+"\n")
-f.close()
+    desc =cont.get("description")
+    print(desc)
+#     f.write(mtname+","+desc+"\n")
+# f.close()
 # f = open('mt_height.csv', 'r', encoding='utf-8')
 
 # df_INNER_JOIN = pd.merge(b, f, left_on='mtname', right_on='mtname', how='inner')
