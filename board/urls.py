1b854ad28bfec63ff3ab6a53de59b0afeafab02d
cks70001559 <cks70001559@gmail.com>
2020-10-21 19:14:34 +0900

    1021 final

--- board/urls.py
+++ board/urls.py
@@ -6,8 +6,8 @@ app_name = 'board'
 
 urlpatterns = [
     path('', views.index),
-    path('list/', views.boardlist, name='boardlist'),
-    path('list/detail/<str:NAME>', views.boardview, name='boardview'),
-    path('goodpricelist/', views.goodpricelist, name='goodpricelist'),
-    path('goodpricelist/detail/<str:ADDRESS>', views.goodpriceview, name='goodpriceview'),
+    path('list/', views.boardlist, name='boardlist'),                                        # 명산 리스트
+    path('list/detail/<str:NAME>', views.boardview, name='boardview'),                       # 명산 상세보기
+    path('goodpricelist/', views.goodpricelist, name='goodpricelist'),                       # 맛집 리스트 
+    path('goodpricelist/detail/<str:ADDRESS>', views.goodpriceview, name='goodpriceview'),   # 맛집 상세보기
 ]
--- board/views.py
+++ board/views.py
@@ -16,17 +16,14 @@ def index(request):
 
 def boardlist(request): 
    
-    data = request.GET.copy()
+    data = request.GET.copy()                                          # 빈 Query Dict 생성
 
-    with MongoClient("mongodb://127.0.0.1:27017/") as client:
-        mt_list = list(client.mt_db.mt_col.find({}))
-        # datas['mt_list'] = mt_list
-    # data2 = {'mt_list' : mt_list}
-    paginator = Paginator(mt_list, 10)
-    page_number = request.GET.get('page', 1)
-    data3 = {'page_obj' : paginator.get_page(page_number)}
-
-    # return render(request, 'board/mtlist_fromdb.html', context=datas)  
+    with MongoClient("mongodb://127.0.0.1:27017/") as client:          # MondoDB 연결
+        mt_list = list(client.mt_db.mt_col.find({}))                   # DB 추출
+    paginator = Paginator(mt_list, 10)                                 # 페이지네이션 
+    page_number = request.GET.get('page', 1)                           # 페이지네이션
+    data3 = {'page_obj' : paginator.get_page(page_number)}             # 페이지네이션 
+                                                                       # 템플릿으로 넘겨주기
     return render(request, 'board/mtlist_fromdb.html', { 'page_obj' : paginator.get_page(page_number)})  
 
 def goodpriceview(request,ADDRESS): 
@@ -53,42 +50,35 @@ def goodpriceview(request,ADDRESS):
     return render(request, 'board/goodpriceview.html', context=datas)        
 
 def boardview(request,NAME):
-    
     datas={}
-    with MongoClient("mongodb://127.0.0.1:27017/") as client:
-        mt_list = list(client.mt_db.mt_col.find({'NAME':NAME}))
+    with MongoClient("mongodb://127.0.0.1:27017/") as client:         # DB 연결해서 Data 받아올 준비
+        mt_list = list(client.mt_db.mt_col.find({'NAME':NAME}))       # 상세보기에 해당하는 특정 데이터 추출
+        name=mt_list[0]['NAME']                                       # NAME 변수 설정 
+        height=mt_list[0]['HEIGHT']                                   # HEIGHT 변수 설정 
+        area=mt_list[0]['AREA']                                       # AREA 변수 설정 
+        no=mt_list[0]['NO']                                           # NO 변수 설정 
+        lat=mt_list[0]['LAT']                                         # LAT 변수 설정 
+        lon=mt_list[0]['LON']                                         # LON 변수 설정 
         
-        name=mt_list[0]['NAME']
-        height=mt_list[0]['HEIGHT']
-        area=mt_list[0]['AREA']
-        no=mt_list[0]['NO']
-        lat=mt_list[0]['LAT']
-        lon=mt_list[0]['LON']
-        print(lat,lon)
         lat_long = [lat, lon]
-        m = folium.Map(lat_long, zoom_start=10)
+        m = folium.Map(lat_long, zoom_start=10)                       # folium 으로 Map 에 시각화 준비
         popText = folium.Html(f'<b>{name}&nbsp;({height}m)</b></br>'+f'<b>{area}</b></br>', script=True)
-        popup = folium.Popup(popText, max_width=2650, show=True)
-        folium.RegularPolygonMarker(location=lat_long, popup=popup).add_to(m)
-        m = m._repr_html_() #  updated
-
-        #data = request.GET.copy()
-        #ma = {'mountain_map': m}
+        popup = folium.Popup(popText, max_width=2650, show=True)                   # popup text 설정
+        folium.RegularPolygonMarker(location=lat_long, popup=popup).add_to(m)      # popup marker 설정
+        m = m._repr_html_() 
         header = {'User-Agent' : 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36'}
-        url = f'http://mtweather.nifos.go.kr/famous/mountainOne?stnId={no}'
-        res = requests.get(url, headers=header).json()
-        cont = res.get("famousMTSDTO")  
-        weather2 = [cont.get("forestAWS10Min")]
-        weather1 = [res.get("others")]    
+        url = f'http://mtweather.nifos.go.kr/famous/mountainOne?stnId={no}' # 실시간 기상정보 파싱 준비
+        res = requests.get(url, headers=header).json()                      # json 데이터 추출
+        cont = res.get("famousMTSDTO")                                      # 데이터 가공
+        weather2 = [cont.get("forestAWS10Min")]                             # 데이터 가공
+        weather1 = [res.get("others")]                                      # 데이터 가공
         
-        
-        datas['mt_list'] = mt_list
-        datas['weather1'] = weather1
-        datas['weather2'] = weather2
-        datas['mountain_map'] = m
-        
+        datas['mt_list'] = mt_list                                          # Dict 으로 템플릿에 넘겨줄 준비
+        datas['weather1'] = weather1                                        # Dict 으로 템플릿에 넘겨줄 준비
+        datas['weather2'] = weather2                                        # Dict 으로 템플릿에 넘겨줄 준비
+        datas['mountain_map'] = m                                           # Dict 으로 템플릿에 넘겨줄 준비
     
-    return render(request, 'board/boardview.html', context=datas)   
+    return render(request, 'board/boardview.html', context=datas)           # 템플릿에 넘겨줌
 
 
 def goodpricelist(request): 
